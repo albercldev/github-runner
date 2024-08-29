fdf0fa13b7da7eca0732683ed207cdec09b11078
albercl <alberto.cuadradol@outlook.es>
2024-08-29 09:52:03 +0200

    Improved copy of files

--- start.sh
+++ start.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 # Copy from .store folder if the files exists to avoid reconfiguring the runner
-if [ -f /actions-runner/.store/.credentials ]; then
-  cp /actions-runner/.store/.credentials .
-fi
-if [ -f /actions-runner/.store/.credentials_rsaparams ]; then
-  cp /actions-runner/.store/.credentials_rsaparams .
-fi
-if [ -f /actions-runner/.store/.runner ]; then
-  cp /actions-runner/.store/.runner .
-fi
-if [ -f /actions-runner/.store/.path ]; then
-  cp /actions-runner/.store/.path .
-fi
-if [ -f /actions-runner/.store/.env ]; then
-  cp /actions-runner/.store/.env .
-fi
-if [ -f /actions-runner/.store/svc.sh ]; then
-  cp /actions-runner/.store/svc.sh .
-fi
-if [ -d /actions-runner/.store/_diag ]; then
-  cp -r /actions-runner/.store/_diag .
-fi
+cp -a /actions-runner/.store/. /actions-runner/
 
 ./config.sh --unattended --url $GH_RUNNER_URL --token $GH_RUNNER_TOKEN --replace --name $GH_RUNNER_NAME --labels $GH_RUNNER_LABELS
 
